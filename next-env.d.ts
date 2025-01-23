0069a0fb705f03590a396c16c1ddda605cefcfaa
irfandi marsya <join.mantap@gmail.com>
2025-01-23 17:01:58 +0700

    Fix: Resolve build errors and favicon issue

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,11 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/image-component" />
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+/// <reference types="next/image-types/global" />
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    "swiper-container": any
-    "swiper-slide": any
-  }
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('postcss').Postcss} */
+module.exports = {
+  plugins: [
+    'tailwindcss', 
+    'postcss-nested', 
+    'autoprefixer'
+  ],
+};
